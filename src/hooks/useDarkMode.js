8e5add949744f37a3fad20fa81bc70f2eeaed32d
Ruth Poliakon <r.poliakon@gmail.com>
2019-10-07 20:36:49 -0600

    MVP complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,14 +3,14 @@ import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 export default function useDarkMode() {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", "false");
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
 
   useEffect(() => {
-    const body = document.getElementsByTagName("body");
+    const body = document.querySelector("body");
     if (darkMode) {
-      body.classlist.add("dark-mode");
+      body.classList.add("dark-mode");
     } else {
-      body.classlist.remove("dark-mode");
+      body.classList.remove("dark-mode");
     }
   }, [darkMode]);
 
